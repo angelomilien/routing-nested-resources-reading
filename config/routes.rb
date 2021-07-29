5ba55f143ff7bba3b829a3a5952e85d71a969ffa
Angelo <angelomilienjoseph@gmail.com>
2021-07-29 16:02:54 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-  root 'posts#index'
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
+
+  root 'posts#index' 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,7 @@ post3 = <<-POST3
 Post.create(title: "Something about Democrats", description: post1, author: author1)
 Post.create(title: "SPAM SPAM SPAM", description: post2, author: author2)
 post = Post.create(title: "Links! Click Links!", description: post3, author: author3)
+Post.create(title: "SPAM SPAM SPAM 2", description: post2, author: author1)
 
 #set one to yesterday
 post.update_attribute(:created_at, Time.zone.today.advance(days: -1))
